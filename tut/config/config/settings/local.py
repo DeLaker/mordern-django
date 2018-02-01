413c669951b4ccbb7e98effb1b5d649c84dfa0e7
DeLaker <subsidyg@gmail.com>
2018-02-01 18:19:13 +0100

    Delete local.py

--- tut/config/config/settings/local.py
+++
@@ -1,7 +0,0 @@
-from .base import *
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_SECRETE_KEY', default='x0(6llyh92h2j8^h&hhx!8#^#l%kiak52g1^u+99n_(2eqed0q')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DJANGO_DEBUG', default=True)
